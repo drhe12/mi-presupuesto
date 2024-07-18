35882e67cb36323df7d245479bd54fdcd4bcf945
drhe12 <deryam.19.07.13@gmail.com>
2024-07-18 17:32:30 -0500

    [Feature] create BudgetDatabase.kt

---
+++ app/src/main/java/com/drhe/mi/presupuesto/data/BudgetDatabase.kt
@@ -0,0 +1,14 @@
+package com.drhe.mi.presupuesto.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.drhe.mi.presupuesto.data.dao.CategoryDao
+import com.drhe.mi.presupuesto.data.dao.TransactionDao
+import com.drhe.mi.presupuesto.data.models.Category
+import com.drhe.mi.presupuesto.data.models.Transaction
+
+@Database(entities = [Transaction::class, Category::class], version = 1, exportSchema = false)
+abstract class BudgetDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+    abstract fun categoryDao(): CategoryDao
+}
---
+++ app/src/main/java/com/drhe/mi/presupuesto/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.drhe.mi.presupuesto.util
+
+object Constants {
+    const val DATABASE_NAME = "budget_db"
+    const val BUDGET_TABLE = "budget_table"
+    const val CATEGORY_TABLE = "category_table"
+    const val TRANSACTION_TABLE = "transaction_table"
+}
