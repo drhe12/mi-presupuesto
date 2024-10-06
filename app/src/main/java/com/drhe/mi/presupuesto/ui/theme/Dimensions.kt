78b5bc23ac50851b4ff1474952de1a64476e7fb1
drhe12 <deryam.19.07.13@gmail.com>
2024-10-06 12:38:18 -0500

    [Feature][BUDGET-1] add values in res

---
+++ app/src/main/java/com/drhe/mi/presupuesto/ui/theme/Dimensions.kt
@@ -0,0 +1,12 @@
+package com.drhe.mi.presupuesto.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val SMALL_PADDING = 8.dp
+val MEDIUM_PADDING = 12.dp
+val LARGE_PADDING = 16.dp
+val EXTRA_LARGE_PADDING = 24.dp
+
+val TOP_APP_BAR_HEIGHT = 56.dp
+
+val ELEVATION = 6.dp
